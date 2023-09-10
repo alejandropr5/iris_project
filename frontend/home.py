b682b8538bc8a8dd1c373fbeeb0c27ca2293d4f8
alejandropr05 <alejandropr05@hotmail.com>
2023-09-10 11:19:06 -0500

    configuring environment API_ENDPOINT

--- frontend/home.py
+++ frontend/home.py
@@ -1,57 +1,66 @@
 import streamlit as st
 import requests
 import json
+import os
 
-url = "http://localhost:8000/iris/predict"
+API_ENDPOINT = os.environ.get("API_ENDPOINT", "https://localhost:8000")
 
 
-def call_api(sepal_length, sepal_width, petal_length, petal_width):
-    request_data = json.dumps([{"sepal_length": sepal_length,
-                                "sepal_width": sepal_width,
-                                "petal_length": petal_length,
-                                "petal_width": petal_width}])
-
-    headers = {'Content-Type': 'application/json'}
-    response = requests.request("POST",
-                                url,
-                                headers=headers,
-                                data=request_data)
+def call_api_predict_method(
+    sepal_length, sepal_width, petal_length, petal_width
+):
+    request_data = [{
+        "sepal_length": sepal_length,
+        "sepal_width": sepal_width,
+        "petal_length": petal_length,
+        "petal_width": petal_width
+    }]
+    request_data_json = json.dumps(request_data)
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    predict_method_endpoint = f"{API_ENDPOINT}/iris/predict"
+    response = requests.request(
+        "POST", predict_method_endpoint,
+        headers=headers, data=request_data_json
+    )
     response_json = response.json()
     predictions = response_json['predictions']
-    return predictions[0]
+    label = predictions[0]
+    return label
 
 
 def app():
-    st.set_page_config(page_title="Streamlit App",
-                       page_icon="🎪",
-                       layout="wide")
-    st.title('Home')
-    st.write('Welcome to the home page!!!')
 
-    sepal_length = st.number_input('Sepal length',
-                                   min_value=0.0,
-                                   max_value=10.0,
-                                   value=5.0)
-    sepal_width = st.number_input('Sepal width',
-                                  min_value=0.0,
-                                  max_value=10.0,
-                                  value=5.0)
-    petal_length = st.number_input('Petal length',
-                                   min_value=0.0,
-                                   max_value=10.0,
-                                   value=5.0)
-    petal_width = st.number_input('Petal width',
-                                  min_value=0.0,
-                                  max_value=10.0,
-                                  value=5.0)
+    st.set_page_config(
+        page_title="Streamlit App",
+        page_icon="🧊",
+        layout="wide"
+    )
+    st.title('Home')
+    st.write('Welcome to the Iris frontend demo!!!')
+
+    sepal_length = st.number_input(
+        'Sepal length', min_value=0.0, max_value=10.0, value=5.0
+    )
+    sepal_width = st.number_input(
+        'Sepal width', min_value=0.0, max_value=10.0, value=5.0
+    )
+    petal_length = st.number_input(
+        'Petal length', min_value=0.0, max_value=10.0, value=5.0
+    )
+    petal_width = st.number_input(
+        'Petal width', min_value=0.0, max_value=10.0, value=5.0
+    )
 
     i_was_clicked = st.button("Predict")
-
     if i_was_clicked:
-        label = call_api(sepal_length, sepal_width, petal_length, petal_width)
+        label = call_api_predict_method(
+            sepal_length, sepal_width, petal_length, petal_width
+        )
         st.write(f'Predicted label: {label}')
         st.balloons()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app()
